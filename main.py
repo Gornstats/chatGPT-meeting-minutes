a4fcfba0a123cc7bf01527d64bc9b2c714f37321
Jeremy Gorniak <railspider@gmail.com>
2023-09-17 20:31:35 +1000

    Security

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import openai
 from docx import Document
+
+def transcribe_audio(audio_file_path):
+    with open(audio_file_path, 'rb') as audio_file:
+        transcription = openai.Audio.transcribe('whisper-1', audio_file)
+    return transcription['text']
+
+
+#transcribe_audio("audio/EarningsCall.wav")
